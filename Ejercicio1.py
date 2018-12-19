c811a4b5230e76d7c08c238653993a23b49d3d2f
jught <37115117+jught@users.noreply.github.com>
2018-12-19 22:10:09 +0100

    Create Ejercicio1.py

---
+++ Ejercicio1.py
@@ -0,0 +1,5 @@
+x = float(input("Introduzca un numero"))
+y = float(input("Introduzca un numero"))
+x += y
+if(x>10):print("La suma de",x,"y",y, " es mayor que 10")
+else:print("La suma de",x,"y",y, " NO es mayor que 10")
